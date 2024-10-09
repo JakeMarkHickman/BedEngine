c7f30af75f9514f95c951a515bb0d8dcb9331470
Mr-Mycotic <Perilousbasher3@gmail.com>
2024-10-09 17:25:37 +0100

    Some changes made

--- BedEngine/Source/Graphics/VertexArray.cpp
+++ BedEngine/Source/Graphics/VertexArray.cpp
@@ -23,6 +23,8 @@ void Bed::VertexArray::AddBuffer(const Bed::VertexBuffer& vb, const Bed::VertexB
     for (unsigned int i = 0; i < elements.size(); i++)
     {
         const Bed::VertexBufferLayoutElement element = elements[i];
+
+        //TODO: Make this not be a cast
         glVertexAttribPointer(i, element.count, element.type, element.normalised, layout.GetStride(), reinterpret_cast<const void*>(uintptr_t(offset))); // This is what links the Array buffer to the Vertex Array
         glEnableVertexAttribArray(i);
         offset += element.count * Bed::VertexBufferLayoutElement::GetSizeOfType(element.type);
