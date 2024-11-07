4686a5250c0b4ec98b70cf9c1366e5ad2d2e981b
Mr-Mycotic <64696110+Mr-Mycotic@users.noreply.github.com>
2024-11-07 00:57:59 +0000

    Update Mesh.cpp

--- BedEngine/Source/Bed/GameObjects/Mesh/Mesh.cpp
+++ BedEngine/Source/Bed/GameObjects/Mesh/Mesh.cpp
@@ -14,11 +14,48 @@ Bed::Mesh::Mesh(std::string path)
     std::ifstream file(path);
     // Loop through each line in the file
     while (std::getline(file, line)) {
-        // Check if the line starts with "v "
-        if (line.rfind("f ", 0) == 0) {
-            ++faceCount;
-        }
+        GrabData(line);
     }
 
     m_Faces.resize(faceCount);
+}
+
+void GrabData(std::string line)
+{
+    if (line.rfind("v ", 0) == 0)
+    {
+
+    }
+    else if (line.rfind("vt ", 0) == 0)
+    {
+
+    }
+    else if (line.rfind("vn ", 0) == 0)
+    {
+
+    }
+    else if (line.rfind("f ", 0) == 0)
+    {
+        
+    }
+}
+
+void Bed::Mesh::LoadVertices(std::string line)
+{
+    
+}
+
+void Bed::Mesh::LoadUVWs(std::string line)
+{
+    
+}
+
+void Bed::Mesh::LoadVertexNormals(std::string line)
+{
+    
+}
+
+void Bed::Mesh::LoadFaces(std::string line)
+{
+    
 }
