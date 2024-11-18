1a9d0e71dd7c75eb15f0319057e3efc8612b194b
Mr-Mycotic <Perilousbasher3@gmail.com>
2024-11-18 21:39:54 +0000

    Split Vectors into separate files

--- BedEngine/Source/Bed/Math/Matrix.h
+++ BedEngine/Source/Bed/Math/Matrix.h
@@ -5,16 +5,30 @@ namespace Bed
     struct Matrix2
     {
         Matrix2();
+
+        float element[2][2];
     };
 
     struct Matrix3
     {
         Matrix3();
+        
+        float element[3][3];
+
+        Matrix3 operator*(float scalar)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                
+            }
+        }
     };
 
     struct Matrix4
     {
         Matrix4();
+
+        float element[4][4];
     };
     
 }
