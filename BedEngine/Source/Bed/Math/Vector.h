bb9e60290bdb16014dca1ed9633c1547dc629ab9
Mr-Mycotic <64696110+Mr-Mycotic@users.noreply.github.com>
2024-11-18 18:11:32 +0000

    Vector2 Operations

--- BedEngine/Source/Bed/Math/Vector.h
+++ BedEngine/Source/Bed/Math/Vector.h
@@ -35,6 +35,142 @@ namespace Bed
         {
             return x != other.x && y != other.y;
         }
+
+        // Is Greater than or Equal
+        bool operator>=(float scalar) const
+        {
+            return x >= scalar && y >= scalar;
+        }
+        bool operator>=(const Vector2& other) const
+        {
+            return x >= other.x && y >= other.y;
+        }
+
+        // Is Less than or Equal
+        bool operator<=(float scalar) const
+        {
+            return x <= scalar && y <= scalar;
+        }
+        bool operator<=(const Vector2& other) const
+        {
+            return x <= other.x && y <= other.y;
+        }
+
+        // Is Greater than
+        bool operator>(float scalar) const
+        {
+            return x > scalar && y > scalar;
+        }
+        bool operator>(const Vector2& other) const
+        {
+            return x > other.x && y > other.y;
+        }
+
+        // Is Less than
+        bool operator<(float scalar) const
+        {
+            return x < scalar && y < scalar;
+        }
+        bool operator<(const Vector2& other) const
+        {
+            return x < other.x && y < other.y;
+        }
+
+        // Multiply and Equal
+        Vector2& operator*=(float scalar)
+        {
+            x *= scalar;
+            y *= scalar;
+            return *this;
+        }
+        Vector2& operator*=(const Vector2& other)
+        {
+            x *= other.x;
+            y *= other.y;
+            return *this;
+        }
+
+        // Divide and Equal
+        Vector2& operator/=(float scalar)
+        {
+            x /= scalar;
+            y /= scalar;
+            return *this;
+        }
+        Vector2& operator/=(const Vector2& other)
+        {
+            x /= other.x;
+            y /= other.y;
+            return *this;
+        }
+
+        // Plus and Equal
+        Vector2& operator+=(float scalar)
+        {
+            x += scalar;
+            y += scalar;
+            return *this;
+        }
+        Vector2& operator+=(const Vector2& other)
+        {
+            x += other.x;
+            y += other.y;
+            return *this;
+        }
+
+        // Subtract and Equal
+        Vector2& operator-=(float scalar)
+        {
+            x -= scalar;
+            y -= scalar;
+            return *this;
+        }
+        Vector2& operator-=(const Vector2& other)
+        {
+            x -= other.x;
+            y -= other.y;
+            return *this;
+        }
+
+        // Multiplication
+        Vector2 operator*(float scalar) const
+        {
+            return Vector2(x * scalar, y * scalar);
+        }
+        Vector2 operator*(const Vector2& other) const
+        {
+            return Vector2(x * other.x, y * other.y);
+        }
+
+        //Division
+        Vector2 operator/(float scalar) const 
+        {
+            return Vector2(x / scalar, y / scalar);
+        }
+        Vector2 operator/(const Vector2& other) const
+        {
+            return Vector2(x / other.x, y / other.y);
+        }
+
+        // Addition
+        Vector2 operator+(float scalar) const
+        {
+            return Vector2(x + scalar, y + scalar);
+        }
+        Vector2 operator+(const Vector2& other) const
+        {
+            return Vector2(x + other.x, y + other.y);
+        }
+
+        // Subtraction
+        Vector2 operator-(float scalar) const 
+        {
+            return Vector2(x - scalar, y - scalar);
+        }
+        Vector2 operator-(const Vector2& other) const
+        {
+            return Vector2(x - other.x, y - other.y);
+        }
     };
 
     struct BED_API Vector3
