ae7f62a004b315414fda9d386db4686170d66eec
Mr-Mycotic <64696110+Mr-Mycotic@users.noreply.github.com>
2024-12-20 02:13:08 +0000

    Started work on child comp

--- BedEngine/Source/Bed/Game/World/GameObjects/Systems/ChildSystem.h
+++ BedEngine/Source/Bed/Game/World/GameObjects/Systems/ChildSystem.h
@@ -11,19 +11,27 @@ namespace Bed
         {
             if(ecs.HasComponents<Bed::Child>(i))
             {
-                Bed::Child child = ecs.GetComponent<Bed::Child>(i);
+                Bed::Child* child = ecs.GetComponent<Bed::Child>(i);
 
-                if(ecs.HasComponents<Bed::Transform>(child.Parent))
+                if(ecs.HasComponents<Bed::Transform>(child->Parent))
                 {
-                    Bed::Transform parentTransform = ecs.GetComponent<Bed::Transform>(child.Parent);
+                    Bed::Transform* parentTransform = ecs.GetComponent<Bed::Transform>(child->Parent);
 
                     if(!ecs.HasComponents<Bed::Transform>(i))
                     {
                         ecs.AttachComponents(i, Bed::Transform(0.0f));
                     }
 
-                    //TODO : Use parents Transform to calculate the childs transform
+                    Bed::Transform* transform = ecs.GetComponent<Bed::Transform>(i);
+
+                    transform->Position = parentTransform->Position + transform->LocalPosition;
                     
+                    //TODO: Rotation needs to rotate from parents local position
+                    transform->Rotation = parentTransform->Rotation + transform->LocalRotation;
+                    transform->Scale = parentTransform->Scale * transform->LocalScale;
+
+                    std::cout << "Child x = " << transform->Position.x << " y = " << transform->Position.y << " z = " << transform->Position.z << "\n";
+                    std::cout << "Parent x = " << parentTransform->Position.x << " y = " << parentTransform->Position.y << " z = " << parentTransform->Position.z << "\n";
                 }
             }
         }
--- BedEngine/Source/Bed/Game/World/World.cpp
+++ BedEngine/Source/Bed/Game/World/World.cpp
@@ -5,22 +5,34 @@
 #include <GameObjects/Components/RenderComponent.h>
 #include <GameObjects/Components/AmbientLightComponent.h>
 #include <GameObjects/Components/DiffuseLightComponent.h>
+#include <GameObjects/Components/ChildComponent.h>
 #include <Bed/Game/World/GameObjects/Components/VelocityComponent.h>
 
 #include <GameObjects/Systems/CameraSystem.h>
 #include <GameObjects/Systems/RenderSystem.h>
 #include <GameObjects/Systems/LightingSystem.h>
+#include <GameObjects/Systems/ChildSystem.h>
 #include <Bed/Game/World/GameObjects/Systems/MovementSystem.h>
 
 Bed::World::World()
 {
     uint64_t Player = ecs.CreateEntity();
-    ecs.AttachComponents(Player, Bed::Transform(Bed::Vector3(0.0f, 0.0f, -5.0f), Bed::Vector3(0.0f), Bed::Vector3(1.0f)),
+    ecs.AttachComponents(Player, Bed::Transform(Bed::Vector3(0.0f, 0.0f, -2.0f), Bed::Vector3(0.0f), Bed::Vector3(1.0f)),
                                  Bed::Render("Assets/Resources/Meshes/Cube.obj"));
 
     uint64_t Camera = ecs.CreateEntity();
     ecs.AttachComponents(Camera, Bed::Transform(Bed::Vector3(0.0f, 0.0f, 0.0f), Bed::Vector3(0.0f), Bed::Vector3(1.0f)),
                                  Bed::Camera(Bed::RenderType::Projection));
+
+    uint64_t Ent1 = ecs.CreateEntity();
+    ecs.AttachComponents(Ent1, Bed::Transform(Bed::Vector3(3.0f, 0.0f, -2.0f), Bed::Vector3(0.0f), Bed::Vector3(1.0f)),
+                              Bed::Render("Assets/Resources/Meshes/Cube.obj"),
+                              Bed::Velocity(Bed::Vector3(0.0f, 0.1f, 0.0f)));
+
+    uint64_t Ent2 = ecs.CreateEntity();
+    ecs.AttachComponents(Ent2, Bed::Transform(Bed::Vector3(-3.0f, 0.0f, -2.0f), Bed::Vector3(0.0f), Bed::Vector3(1.0f)),
+                               Bed::Render("Assets/Resources/Meshes/Cube.obj"),
+                               Bed::Child(Ent1));
 
     uint64_t Ambient = ecs.CreateEntity();
     ecs.AttachComponents(Ambient, Bed::AmbientLight(0.1f, Bed::Vector3(1.0f, 1.0f, 1.0f)));
@@ -32,6 +44,7 @@ Bed::World::World()
     ecs.AddSystem(CameraSystem);
     ecs.AddSystem(RenderSystem);
     ecs.AddSystem(LightingSystem);
+    ecs.AddSystem(ChildSystem);
 }
 
 Bed::World::~World()
